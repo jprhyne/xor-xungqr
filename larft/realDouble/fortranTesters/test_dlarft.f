301877ffb13ee903f2a1480ba2543147e31776b3
Johnathan Rhyne <rhynejohnathan@gmail.com>
2024-09-17 16:30:48 -0600

    fixing errors from previous attempt. Now works for cases with FC and FR

--- larft/realDouble/fortranTesters/test_dlarft.f
+++ larft/realDouble/fortranTesters/test_dlarft.f
@@ -8,7 +8,8 @@
          CHARACTER         STOREV, DIRECT
          ! Local arrays
          DOUBLE PRECISION, ALLOCATABLE :: A(:,:), Q(:,:), As(:,:), 
-     $            Qs(:,:), WORKMAT(:,:), WORK(:), T(:,:)
+     $            Qs(:,:), WORKMAT(:,:), WORK(:), T(:,:), Qt(:,:),
+     $            Qts(:,:), Ts(:,:)
 
          DOUBLE PRECISION, DIMENSION(:), ALLOCATABLE :: TAU
 
@@ -26,10 +27,13 @@
          ALLOCATE(A(M,N))
          ALLOCATE(As(M,N))
          ALLOCATE(Q(M,N))
-         ALLOCATE(Qs(M, N))
+         ALLOCATE(Qs(M,N))
+         ALLOCATE(Qt(N,M))
+         ALLOCATE(Qts(N,M))
          ALLOCATE(WORK(1))
          ALLOCATE(TAU(N))
          ALLOCATE(T(N,N))
+         ALLOCATE(Ts(N,N))
          ! Generate a random A
          CALL RANDOM_NUMBER(A)
 
@@ -48,11 +52,13 @@
          CALL DLACPY('All', M, N, A, M, Qs, M)
 
          DEALLOCATE(WORK)
+c----------------------------------------------------------------------
          ! Compute the triangular factor T
-         STOREV = 'C'
          DIRECT = 'F'
+         STOREV = 'C'
          CALL MY_DLARFT_REC(DIRECT, STOREV, M, N, Q, M, TAU, Q, M)
 
+         ! Test the results
          ! Now call MY_DORGKR
          CALL MY_DORGKR(M, N, Q, M)
          
@@ -84,11 +90,89 @@
 
          DEALLOCATE(WORKMAT)
 
-         WRITE(*,*) "Recursive DLARFT"
+         WRITE(*,*) "Recursive DLARFT. Forward, Column"
 
          WRITE(*,*) "representation norm: ", NORM_REPRES
          WRITE(*,*) "orthogonal norm:     ", NORM_ORTH
 
+         ! Copy Qs back into Q
+         CALL DLACPY('ALL', M, N, Qs, M, Q, M)
+c----------------------------------------------------------------------
+         ! Above but STOREV = 'R'
+         DIRECT = 'F'
+         STOREV = 'R'
+         ! Copy Qs into Qt
+         DO I = 1, M
+            DO J = 1, N
+               Qt(J,I) = Q(I,J)
+            END DO
+         END DO
+         ! Set T to be all 0s
+         DO I = 1, N
+            DO J = 1, N
+               T(I,J) = ZERO
+               Ts(I,J)= ZERO
+            END DO
+         END DO
+         ! Call my dlarft implementation
+         CALL MY_DLARFT_REC(DIRECT, STOREV, M, N, Qt, N, TAU, T, N)
+         ! Ensure that T is upper triangular
+         NORM_REPRES = 0
+         DO I = 2, N
+            DO J = 1, I-1
+               NORM_REPRES = NORM_REPRES + T(I,J) * T(I,J)
+               IF(T(I,J).NE.ZERO) THEN
+                  WRITE(*,*) "I = ", I, "J = ", J
+               END IF
+            END DO
+         END DO
+         IF(NORM_REPRES.NE.ZERO) THEN
+            WRITE(*,*) "The upper triangular part of T was touched"
+            RETURN
+         END IF
+         ! Getting to this stage means that T is lower triangular. 
+         ! So, we copy T into the upper part of Q
+         CALL DLACPY('Upper', N, N, T, N, Q, M)
+         ! Next, we check how accurate T was!
+         CALL MY_DORGKR(M, N, Q, M)
+         
+         ALLOCATE(WORKMAT(N,N))
+         CALL DLASET('A', N,N, ZERO, ZERO, WORKMAT, N)
+         CALL DSYRK('Upper', 'Transpose', N, M, ONE, Q, M, ZERO,
+     $         WORKMAT, N)
+
+         DO I = 1, N
+            WORKMAT(I,I) = WORKMAT(I,I) - 1
+         END DO
+
+         NORM_ORTH = DLANGE('Frobenius', N, N, WORKMAT, N, WORK)
+
+         DEALLOCATE(WORKMAT)
+         ALLOCATE(WORKMAT(M,N))
+         CALL DLACPY('All', M, N, Q, M, WORKMAT, M)
+         CALL DTRMM('Right', 'Upper', 'No-transpose', 'non-unit',
+     $      M, N, ONE, A, M, WORKMAT, M)
+
+         DO I = 1, M
+            DO J = 1, N
+               WORKMAT(I,J) = WORKMAT(I,J) - As(I,J)
+            END DO
+         END DO
+         NORM_REPRES = DLANGE('Frobenius', M, N, WORKMAT,
+     $      M, WORK)
+         NORM_REPRES = NORM_REPRES / NORMA
+
+         DEALLOCATE(WORKMAT)
+
+         WRITE(*,*) "Recursive DLARFT. Forward, Row"
+
+         WRITE(*,*) "representation norm: ", NORM_REPRES
+         WRITE(*,*) "orthogonal norm:     ", NORM_ORTH
+
+         ! Copy Qs back into Q
+         CALL DLACPY('ALL', M, N, Qs, M, Q, M)
+
+c----------------------------------------------------------------------
          ! Copy Qs back into Q
          CALL DLACPY('ALL', M, N, Qs, M, Q, M)
 
