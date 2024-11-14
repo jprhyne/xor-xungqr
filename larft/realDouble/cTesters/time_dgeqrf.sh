2e89eeadc5636526e6b73c9b7973f514dc0e3557
Johnathan Rhyne <rhynejohnathan@gmail.com>
2024-11-14 10:34:28 -0700

    uploading new experiments to make figures

--- larft/realDouble/cTesters/time_dgeqrf.sh
+++ larft/realDouble/cTesters/time_dgeqrf.sh
@@ -1,7 +1,7 @@
 minM=5000
 maxM=50000
 incM=1000
-echo "Current case of 32 block size"
+echo "Tall current case of 32 block size"
 for (( m=$minM; m<$maxM; m+=$incM ))
 do
     echo "m=$m n=$minM nb=32"
@@ -11,11 +11,24 @@ do
         ./timeDgeqrf.exe -m $m -n $minM -nb 32
     done
 done
+echo "Wide current case of 32 block size"
+minN=10
+maxN=2000
+incN=100
+for (( n=$minN; n<$maxN; n+=$incN ))
+do
+    echo "m=$minM n=$n nb=32"
+    echo "Testing file ran 10 times"
+    for (( l=1; l<= 10; l+=1 ))
+    do
+        ./timeDgeqrf.exe -m $minM -n $n -nb 32
+    done
+done
 echo "Trying new block sizes"
 # fix m and vary n
 m=10000
-maxN=2048
-for (( n=2; n<=maxN ; n*=2 ))
+maxN=8192
+for (( n=16; n<=maxN ; n*=2 ))
 do
     echo "m=$m n=$m nb=$n"
     echo "Testing file ran 10 times"
