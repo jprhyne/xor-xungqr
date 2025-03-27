c4ef022e1885d7d19a6f539bf5eed8dcc1b6ea14
Johnathan Rhyne <rhynejohnathan@gmail.com>
2025-03-27 14:51:17 -0600

    Checkpoint for terminating case implementation

--- 2r/realDouble/src/dtrmmoop.f
+++ 2r/realDouble/src/dtrmmoop.f
@@ -1,3 +1,200 @@
+*> \brief \b DTRMMOOP computes an out of place triangular times general matrix multiplication
+*
+*  =========== DOCUMENTATION ===========
+*
+*  Online html documentation available at
+*            http://www.netlib.org/lapack/explore-html/
+*
+*  Definition:
+*  ===========
+*
+*     RECURSIVE SUBROUTINE DTRMMOOP(SIDE, UPLO, TRANSA, TRANSB,
+*    $         DIAG, M, N, ALPHA, A, LDA, B, LDB, BETA, C, LDC)
+*
+*        .. Scalar Arguments ..
+*        DOUBLE PRECISION  ALPHA, BETA
+*        INTEGER           M, N, LDA, LDB, LDC
+*        CHARACTER         SIDE, UPLO, TRANSA, TRANSB, DIAG
+*        ..
+*        .. Array Arguments ..
+*        DOUBLE PRECISION  A(LDA,*), B(LDB,*), C(LDC,*)
+*        ..
+*
+*> \par Purpose:
+*  =============
+*>
+*> \verbatim
+*>
+*> DTRMMOOP performs one  of the matrix-matrix operatiions
+*>
+*>       C = \alpha op(A) * op(B) + \beta C
+*>                      or
+*>       C = \alpha op(B) * op(A) + \beta C
+*>
+*> where \alpha and \beta are scalars, C is an m-by-n matrix, B is
+*> a unit, or non-unit, upper or lower triangular matrix, and op(B) is
+*> is one of
+*>
+*>       op(B) = B      or       op(B) = B**T
+*> \endverbatim
+*
+*  Arguments:
+*  ==========
+*
+*> \param[in] SIDE
+*> \verbatim
+*>          SIDE is CHARACTER*1
+*>           On entry, SIDE specifies whether op(B) multiplies op(A) from
+*>           the left or right as follows:
+*>
+*>             SIDE = 'L' or 'l'    C = \alpha op(B) * op(A) + \beta C
+*>
+*>             SIDE = 'R' or 'r'    C = \alpha op(A) * op(B) + \beta C
+*> \endverbatim
+*>
+*> \param[in] UPLO
+*> \verbatim
+*>          UPLO is CHARACTER*1
+*>           On entry, UPLO specifies whether the matrix B is an upper or
+*>           lower triangular matrix as follows:
+*>             UPLO = 'U' or 'u'    B is upper triangular
+*>
+*>             UPLO = 'L' or 'l'    B is lower triangular
+*> \endverbatim
+*>
+*> \param[in] TRANSA
+*> \verbatim
+*>          TRANSA is CHARACTER*1
+*>           On entry, TRANSA specifies the form of op(A) to be used in
+*>           the matrix multiplication as follows:
+*>             TRANSA = 'N' or 'n'    op(A) = A
+*>
+*>             TRANSA = 'T' or 't'    op(A) = A**T
+*>
+*>             TRANSA = 'C' or 'c'    op(A) = A**T
+*> \endverbatim
+*>
+*> \param[in] TRANSB
+*> \verbatim
+*>          TRANSB is CHARACTER*1
+*>           On entry, TRANSB specifies the form of op(B) to be used in
+*>           the matrix multiplication as follows:
+*>             TRANSB = 'N' or 'n'     op(B) = B
+*>
+*>             TRANSB = 'T' or 't'     op(B) = B**T
+*>
+*>             TRANSB = 'C' or 'c'     op(B) = B**T
+*> \endverbatim
+*>
+*> \param[in] DIAG
+*> \verbatim
+*>          DIAG is CHARACTER*1
+*>           On entry, DIAG specifies whether or not B is unit triangular
+*>           as follows:
+*>
+*>              DIAG = 'U' or 'u'      B is assumed to be unit triangular.
+*>
+*>              DIAG = 'N' or 'n'      B is not assumed to be unit
+*>                                  triangular.
+*> \endverbatim
+*>
+*> \param[in] M
+*> \verbatim
+*>          M is INTEGER
+*>           On entry, M specifies the number of rows of C. M must be at
+*>           least zero.
+*> \endverbatim
+*>
+*> \param[in] N
+*> \verbatim
+*>          N is INTEGER
+*>           On entry, N specifies the number of columns of C. N must be
+*>           at least zero.
+*> \endverbatim
+*>
+*> \param[in] ALPHA
+*> \verbatim
+*>          ALPHA is DOUBLE PRECISION.
+*>           On entry, ALPHA specifies the scalar alpha. When alpha is
+*>           zero then A and B are not referenced, and A and B need not
+*>           be set before entry.
+*> \endverbatim
+*>
+*> \param[in] A
+*> \verbatim
+*>           A is DOUBLE PRECISION array, dimension ( LDA, K ), where K is M
+*>           If SIDE='R' and TRANSA='N', or SIDE='L' and TRANSA='T' and N
+*>           otherwise. On entry, the leading k-by-k submatrix must contain
+*>           A.
+*> \endverbatim
+*>
+*> \param[in] LDA
+*> \verbatim
+*>          LDA is INTEGER
+*>           On entry, LDA specifies the first dimension of A as declared
+*>           in the calling (sub) program.  When  SIDE = 'R' and TRANSA='N'
+*>           then LDA  must be at least  max( 1, m ), when SIDE = 'R'
+*>           and TRANSA = 'T' then LDA must be at least max( 1, n ).
+*> \endverbatim
+*>
+*> \param[in] B
+*> \verbatim
+*>          B is DOUBLE PRECISION array, dimension ( LDB, K ) where
+*>           K is M when SIDE = 'L' and K is N when SIDE='R'
+*>           Before entry with UPLO = 'U' or 'u', the leading k-by-k
+*>           upper triangular part of the array B must contain the upper
+*>           triangular matrix and the strictly lower triangular part of
+*>           B is not referenced.
+*>           Before entry  with  UPLO = 'L' or 'l', the leading k-by-k
+*>           lower triangular part of the array B must contain the lower
+*>           triangular matrix and the strictly upper triangular part of
+*>           B is not referenced.
+*>           Note that when  DIAG = 'U' or 'u',  the diagonal elements of
+*>           B  are not referenced either,  but are assumed to be  unity.
+*> \endverbatim
+*>
+*> \param[in] LDB
+*> \verbatim
+*>          LDB is INTEGER
+*>           On entry, LDB specifies the first dimension of B as declared
+*>           in the calling (sub) program. When SIDE = 'L' or 'l' then
+*>           LDB must be at least max( 1, m ), when  SIDE = 'R' or 'r'
+*>           then LDB must be at least max( 1, n ).
+*> \endverbatim
+*>
+*> \param[in] BETA
+*> \verbatim
+*>          BETA is DOUBLE PRECISION.
+*>           On entry, BETA specifies the scalar beta. When beta is
+*>           zero then C is not referenced on entry, and C need not
+*>           be set before entry.
+*> \endverbatim
+*>
+*> \param[in,out] C
+*> \verbatim
+*>          C is DOUBLE PRECISION array, dimension ( LDB, N )
+*>           Before entry, the leading m-by-n part of the array C must
+*>           contain the matrix C, and on exit is overwritten by the
+*>           transformed matrix.
+*> \endverbatim
+*>
+*> \param[in] LDC
+*> \verbatim
+*>          LDC is INTEGER
+*>           On entry, LDC specifies the first dimension of C as declared
+*>           in the calling (sub) program. LDC must be at least
+*>           max( 1, m ).
+*> \endverbatim
+*
+*  Authors:
+*  ========
+*
+*> \author Univ. of Tennessee
+*> \author Univ. of California Berkeley
+*> \author Univ. of Colorado Denver
+*> \author NAG Ltd.
+*
+*  =====================================================================
       RECURSIVE SUBROUTINE DTRMMOOP(SIDE, UPLO, TRANSA, TRANSB,
      $         DIAG, M, N, ALPHA, A, LDA, B, LDB, BETA, C, LDC)
 *
@@ -13,35 +210,148 @@
 *  =====================================================================
 *
 *        .. External Functions ..
-         LOGICAL  LSAME
-         EXTERNAL LSAME
+         LOGICAL           LSAME
+         DOUBLE PRECISION  DDOT
+         EXTERNAL          LSAME, DDOT
 *        ..
 *        .. External Subroutines ..
+         EXTERNAL          DGEMM
 *        ..
 *        .. Intrinsic Functions ..
+         INTRINSIC         MIN
 *        ..
 *        .. Local Scalars ..
-         INTEGER  L, K
-         LOGICAL  LSIDE, UPPER, UNIT, TRANST, TRANSG
+         INTEGER           I, L, K, INCA, OFFB
+         LOGICAL           LSIDE, UPPER, UNIT, TRANST, TRANSG
 *        ..
 *        .. Local Parameters ..
-         DOUBLE PRECISION  ONE
-         PARAMETER(ONE=1.0D+0)
-
+         DOUBLE PRECISION  ONE, ZERO
+         PARAMETER(ONE=1.0D+0, ZERO=0.0D+0)
 *        ..
 *
 *        Beginning of Executable Statements
 *
          LSIDE = LSAME(SIDE, 'L')
          UPPER = LSAME(UPLO, 'U')
-         ! If we are transposing the triangular matrix (A)
-         TRANST= LSAME(TRANSA, 'T').OR.LSAME(TRANSA, 'C')
-         ! If we are transposing the general matrix (B)
-         TRANSG= LSAME(TRANSB, 'T').OR.LSAME(TRANSB, 'C')
+         ! If we are transposing the triangular matrix (B)
+         TRANST= LSAME(TRANSB, 'T').OR.LSAME(TRANSB, 'C')
+         ! If we are transposing the general matrix (A)
+         TRANSG= LSAME(TRANSA, 'T').OR.LSAME(TRANSA, 'C')
 *
 *        Terminating Case
 *
          UNIT  = LSAME(DIAG, 'U')
+         IF (M.EQ.1.AND.N.EQ.1) THEN
+*
+*           This case is the simplest as we are just computing C = \alpha A*B +
+*           \beta C where all components are 1-by-1 matrices
+*
+
+            IF(ALPHA.EQ.ZERO) THEN
+               IF(BETA.EQ.ZERO) THEN
+                  C(1,1) = ZERO
+               ELSE
+                  C(1,1) = BETA*C(1,1)
+               END IF
+            ELSE
+               IF (UNIT) THEN
+                  IF(BETA.EQ.ZERO) THEN
+                     C(1,1) = ALPHA*A(1,1)
+                  ELSE
+                     C(1,1) = ALPHA*A(1,1) + BETA*C(1,1)
+                  END IF
+               ELSE
+                  IF(BETA.EQ.ZERO) THEN
+                     C(1,1) = ALPHA*A(1,1)*B(1,1)
+                  ELSE
+                     C(1,1) = ALPHA*A(1,1)*B(1,1) + BETA*C(1,1)
+                  END IF
+               END IF
+            END IF
+         ELSE IF (M.EQ.1) THEN
+*
+*           This means that C is a row vector. If BETA is 0, then we
+*           set it explicitly, otherwise we overwrite it with BETA*C
+*
+            IF (BETA.EQ.0) THEN
+               ! This ensures we don't reference C unless we need to
+               CALL DLASET(M, N, ZERO, ZERO, C, LDC)
+            ELSE
+               CALL DSCAL(N, BETA, C, LDC)
+            END IF
+            IF (ALPHA.NE.ZERO) THEN
+*
+*              Recall that the number of columns of B is determined by SIDE
+*
+               IF (SIDEL) THEN
+*
+*                 Determine if A is a row or column vector
+*
+                  IF (TRANSG) THEN
+                     INCA = 1   ! This means that A is a column vector
+                  ELSE
+                     INCA = LDA ! This means that A is a row vector
+                  END IF
+*
+*                 This means that B is a scalar, so it is either assumed to be
+*                 ONE or explicitly stored in B(1,1)
+*
+                  IF (UNIT) THEN
+                     CALL DAXPY(N, ONE, A, INCA, C, LDC)
+                  ELSE
+                     CALL DAXPY(N, B(1,1), A, INCA, C, LDC)
+                  END IF
+               ELSE
+*
+*                 Determine if A is a row or column vector
+*
+                  IF (TRANSG) THEN
+                     INCA = LDA  ! This means that A is a column vector
+                  ELSE
+                     INCA = 1    ! This means that A is a row vector
+                  END IF
+*
+*                 Determine the length of the columns of B. If it is unitary,
+*                 then we must ignore the diagonal element
+*
+                  IF (DIAG) THEN
+                     OFFB = 1
+                  ELSE
+                     OFFB = 0
+                  END IF
+*
+*                 This means that B is an n-by-n matrix
+*
+                  IF (UPPER) THEN
+                     IF (TRANST) THEN
+*
+*                       TODO: Think of a better way to do this that doesn't
+*                       manually touch A
+*
+                     ELSE
+                        DO I = 1, N
+                           C(1, I) =
+     $                     ALPHA * DDOT(I-OFFB, B(1,I), 1, A, INCA)
+     $                     + C(1,I)
+                        END DO
+*
+*                       If B was assumed unitary we need to add A to C. Otherwise
+*                       we are done!
+*
+                        IF (DIAG) THEN
+                           CALL DAXPY(N, ONE, A, INCA, C, LDA)
+                        END IF
+                     END IF
+                  ELSE
+                     IF (TRANST) THEN
+                     ELSE
+                     END IF
+                  END IF
+               END IF
+            END IF
+         ELSE IF (N.EQ.1) THEN
+*
+         END IF
 *
 *        Recursive Case
 *
